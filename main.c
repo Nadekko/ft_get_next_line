76d91a627d7f80698b5b96b127ec150c44f176ce
Nadekko <139667516+Nadekko@users.noreply.github.com>
2023-11-28 20:12:04 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: andjenna <andjenna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 13:31:25 by andjenna          #+#    #+#             */
-/*   Updated: 2023/11/27 20:06:40 by andjenna         ###   ########.fr       */
+/*   Updated: 2023/11/28 20:05:13 by andjenna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,31 @@
 int	main(int ac, char **av)
 {
 	int		fd;
+	int		fd2;
 
-	fd = open(av[ac - 1], O_RDONLY);
+	(void)ac;
+	fd = open(av[1], O_RDONLY);
+	fd2 = open(av[2], O_RDONLY);
 	if (fd == -1)
 		printf("error");
+	if (fd2 == -1)
+		printf("error");
 	char *str = get_next_line(fd);
-	int i = 0;
-	while (str && i++ < 5)
+	char *str2 = get_next_line(fd2);
+	while (str)
 	{
 		printf("line = %s", str);
 		free(str);
 		str = get_next_line(fd);
 	}
+	printf("\n======================\n");
+	while (str2)
+	{
+		printf("line = %s", str2);
+		free(str2);
+		str2 = get_next_line(fd2);
+	}
+	free(str2);
 	free(str);
 	close(fd);
 	return (0);
